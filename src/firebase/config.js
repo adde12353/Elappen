92227a10a12c8bc8238bba07e3bd1588171c8a4a
Andreas Larsson <Andreas.per.larsson@gmail.com>
2023-05-08 21:31:21 +0200

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxEtKY6qsNf8IYzFE2-tW6wt6N1Z81Hgw",
-  authDomain: "elappen-cfb8e.firebaseapp.com",
-  projectId: "elappen-cfb8e",
-  storageBucket: "elappen-cfb8e.appspot.com",
-  messagingSenderId: "891366742568",
-  appId: "1:891366742568:web:645b01ca27ea38a9b9331d"
+  apiKey: "AIzaSyAwszyQ24dLVK-gzXlhxXv7qp-Om5_V_Nw",
+  authDomain: "udemy-vue-firebase-77cd1.firebaseapp.com",
+  projectId: "udemy-vue-firebase-77cd1",
+  storageBucket: "udemy-vue-firebase-77cd1.appspot.com",
+  messagingSenderId: "863404002760",
+  appId: "1:863404002760:web:80e52e23e7ff925e80c4fd"
 };
 
 
